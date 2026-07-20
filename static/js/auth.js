0e6b526e52a74c290edda5d13cae4d2141e7c4e1
Matheus Machado <marilenemateus750@gmail.com>
2026-07-19 23:33:01 -0300

    JavaScript para mudança visual de telas

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,3 +1,15 @@
+// JS para troca visual entre telas
+
 const authContainer = document.getElementById("authContainer");
 const mostrarCadastro = document.getElementById("mostrarCadastro");
 const mostrarLogin = document.getElementById("mostrarLogin");
+
+mostrarCadastro.addEventListener("click", () => {
+    authContainer.classList.remove("modo-login");
+    authContainer.classList.add("modo-cadastro");
+});
+
+mostrarLogin.addEventListener("click", () => {
+    authContainer.classList.remove("modo-cadastro");
+    authContainer.classList.add("modo-login");
+});
